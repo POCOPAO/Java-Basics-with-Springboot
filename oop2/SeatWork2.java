f2c795594bf299884a3d581da663547a652139b0
Paolo D. Naranjo <pdnaranjo@headoffice.corp.bpi.com.ph>
2025-12-04 16:16:16 +0800

    M2_Activity2

---
+++ oop2/SeatWork2.java
@@ -0,0 +1,12 @@
+package com.java.oop2;
+
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+
+}
---
+++ oop2/WhiteBoard.java
@@ -0,0 +1,9 @@
+package com.java.oop2;
+
+public class WhiteBoard {
+	int sum;
+	int addNumbers(int addend1, int addend2) {
+		sum = addend1 + addend2;
+		return sum;
+	}
+}
