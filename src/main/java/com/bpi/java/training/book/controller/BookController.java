e170f4c10317188932317724ed1c2c3220b311e9
Paolo D. Naranjo <pdnaranjo@headoffice.corp.bpi.com.ph>
2026-03-10 17:39:06 +0800

    M8_Exercise8.5

--- src/main/java/com/bpi/java/training/book/controller/BookController.java
+++ src/main/java/com/bpi/java/training/book/controller/BookController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bpi.java.training.book.model.Book;
@@ -32,6 +33,7 @@ public class BookController {
 	
 	// GET /api/books
 	@GetMapping
+	@ResponseBody
 	public List<Book> getAllBooks(){
 		return books;
 	}
