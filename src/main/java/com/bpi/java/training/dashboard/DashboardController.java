03d1d9dd4ced0fc4225a9ee3848d8a9cda249901
Paolo D. Naranjo <pdnaranjo@headoffice.corp.bpi.com.ph>
2026-03-11 16:19:42 +0800

    M9_Exercise1

---
+++ src/main/java/com/bpi/java/training/dashboard/DashboardController.java
@@ -0,0 +1,13 @@
+package com.bpi.java.training.dashboard;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DashboardController {
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "Welcome to the dashboard!";
+	}
+}
--- src/test/java/com/bpi/java/training/dashboard/DashboardApplicationTests.java
+++ src/test/java/com/bpi/java/training/dashboard/DashboardApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bpi.java.training.book;
+package com.bpi.java.training.dashboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookApplicationTests {
+class DashboardApplicationTests {
 
 	@Test
 	void contextLoads() {
