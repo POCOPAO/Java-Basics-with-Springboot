b39bda33dcd926784ef79f0b57d86e2cb6ec6809
Paolo D. Naranjo <pdnaranjo@headoffice.corp.bpi.com.ph>
2026-03-18 16:02:51 +0800

    M9_Exercise5

--- src/main/java/com/bpi/java/training/dashboard/config/SecurityConfig.java
+++ src/main/java/com/bpi/java/training/dashboard/config/SecurityConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,13 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET,"/home").hasAnyRole("USER","MANAGER")
-				.requestMatchers(HttpMethod.GET,"/dashboard").hasRole("USER")
-				.requestMatchers(HttpMethod.GET,"/reports").hasRole("MANAGER")
+		http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET,"/public").permitAll()
 				.anyRequest().authenticated()).formLogin(Customizer.withDefaults())
 		.httpBasic(Customizer.withDefaults());
 		return http.build();
--- src/main/java/com/bpi/java/training/dashboard/controller/ReportsController.java
+++ src/main/java/com/bpi/java/training/dashboard/controller/ReportsController.java
@@ -1,11 +1,13 @@
 package com.bpi.java.training.dashboard.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReportsController {
 	
+@PreAuthorize("hasRole('MANAGER')")	
 	@GetMapping("/reports")
 	public String reports() {
 		return "Manager reports";
