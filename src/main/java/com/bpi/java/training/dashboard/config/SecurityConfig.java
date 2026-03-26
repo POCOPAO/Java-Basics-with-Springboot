a7fc4ee1583adff85ccc9649f178410a8fcd4bf2
Paolo D. Naranjo <pdnaranjo@headoffice.corp.bpi.com.ph>
2026-03-26 11:57:24 +0800

    M9_Exercise6

--- src/main/java/com/bpi/java/training/dashboard/config/SecurityConfig.java
+++ src/main/java/com/bpi/java/training/dashboard/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,8 +22,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-	@Autowired
-	private CustomUserDetailsService userDetailsService;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,21 +32,13 @@ public class SecurityConfig {
 				.requestMatchers("/profile").hasRole("USER")
 				.anyRequest().authenticated()
 				)
-		.httpBasic(Customizer.withDefaults())
-		.authenticationProvider(authenticationProvider());
+		.httpBasic(Customizer.withDefaults());
 		
 		return http.build();
 	}
 	
 	
 	
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-	DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-	provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(passwordEncoder());
-		return provider;
-	}
 
 
 	
